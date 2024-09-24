46666b572168f5125d9f2686ee2d2120e9c6e98e
Fransoa2003 <fransoa2003@gmail.com>
2024-09-24 01:15:52 -0600

    archivos actualizados

--- Inventory-control/src/Main.java
+++ Inventory-control/src/Main.java
@@ -25,12 +25,58 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel5 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        Panel_shop = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel8 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        label1 = new java.awt.Label();
+        label2 = new java.awt.Label();
+        Panel_Inventory = new javax.swing.JPanel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel12 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jLabel15 = new javax.swing.JLabel();
+        jTextField6 = new javax.swing.JTextField();
+        jLabel16 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        label3 = new java.awt.Label();
+        label4 = new java.awt.Label();
+        label5 = new java.awt.Label();
+        jComboBox3 = new javax.swing.JComboBox<>();
+        jPanel6 = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jTextField7 = new javax.swing.JTextField();
+        jTextField8 = new javax.swing.JTextField();
+        jLabel19 = new javax.swing.JLabel();
+        jTextField9 = new javax.swing.JTextField();
+        jLabel20 = new javax.swing.JLabel();
+        jTextField10 = new javax.swing.JTextField();
+        label6 = new java.awt.Label();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+
+        jLabel5.setText("jLabel5");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
@@ -53,6 +99,7 @@ public class Main extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_supplier.png"))); // NOI18N
         jLabel2.setText("Supplier");
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 150, 30));
 
         jLabel3.setBackground(new java.awt.Color(255, 255, 255));
@@ -60,6 +107,7 @@ public class Main extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_shopping.png"))); // NOI18N
         jLabel3.setText("Shopping");
+        jLabel3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, 150, 30));
 
         jLabel4.setBackground(new java.awt.Color(255, 255, 255));
@@ -67,14 +115,241 @@ public class Main extends javax.swing.JFrame {
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_inventory.png"))); // NOI18N
         jLabel4.setText("Inventory");
+        jLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 150, 30));
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 200, 480));
+
+        jPanel3.setLayout(new java.awt.CardLayout());
+
+        Panel_shop.setBackground(new java.awt.Color(0, 0, 0));
+        Panel_shop.setForeground(new java.awt.Color(255, 255, 255));
+        Panel_shop.setInheritsPopupMenu(true);
+        Panel_shop.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel6.setFont(new java.awt.Font("Sylfaen", 0, 24)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel6.setText("Shopping control");
+        Panel_shop.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 20, -1, -1));
+
+        jLabel7.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Quantity");
+        Panel_shop.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
+
+        jComboBox1.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.setBorder(null);
+        Panel_shop.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 130, 120, 30));
+
+        jLabel8.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Name");
+        Panel_shop.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 70, -1, -1));
+
+        jComboBox2.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox2.setBorder(null);
+        Panel_shop.add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 70, 460, 30));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "Name", "Description", "Quantity", "Total"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setResizable(false);
+            jTable1.getColumnModel().getColumn(0).setPreferredWidth(10);
+        }
+
+        Panel_shop.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 590, 250));
+
+        label1.setText("label1");
+        Panel_shop.add(label1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 130, -1, -1));
+
+        label2.setAlignment(java.awt.Label.CENTER);
+        label2.setBackground(new java.awt.Color(0, 153, 102));
+        label2.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        label2.setForeground(new java.awt.Color(255, 255, 255));
+        label2.setText("Buy now");
+        Panel_shop.add(label2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 120, 130, 40));
+
+        jPanel3.add(Panel_shop, "card4");
+
+        Panel_Inventory.setBackground(new java.awt.Color(0, 0, 0));
+        Panel_Inventory.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel9.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("Inventory");
+        Panel_Inventory.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, -1, -1));
+
+        jLabel11.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setText("Name");
+        Panel_Inventory.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, -1, -1));
+
+        jTextField1.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        Panel_Inventory.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 80, 180, 30));
+
+        jLabel12.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel12.setText("Brand");
+        Panel_Inventory.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, -1, -1));
+
+        jTextField2.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        Panel_Inventory.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 120, 180, 30));
+
+        jTextField4.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        Panel_Inventory.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 80, 180, 30));
+
+        jLabel13.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel13.setText("Description");
+        Panel_Inventory.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 80, -1, -1));
+
+        jLabel14.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel14.setText("Supplier");
+        Panel_Inventory.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 120, -1, -1));
+
+        jTextField5.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        Panel_Inventory.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 160, 60, 30));
+
+        jLabel15.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel15.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel15.setText("Quantity");
+        Panel_Inventory.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, -1));
+
+        jTextField6.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        Panel_Inventory.add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 160, 60, 30));
+
+        jLabel16.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel16.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel16.setText("Size");
+        Panel_Inventory.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 160, -1, -1));
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        Panel_Inventory.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(22, 300, 620, 160));
+
+        label3.setAlignment(java.awt.Label.CENTER);
+        label3.setBackground(new java.awt.Color(153, 0, 255));
+        label3.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        label3.setText("Modify");
+        Panel_Inventory.add(label3, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 210, 130, 40));
+
+        label4.setAlignment(java.awt.Label.CENTER);
+        label4.setBackground(new java.awt.Color(0, 153, 51));
+        label4.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        label4.setText("Add");
+        Panel_Inventory.add(label4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 210, 130, 40));
+
+        label5.setAlignment(java.awt.Label.CENTER);
+        label5.setBackground(new java.awt.Color(255, 51, 51));
+        label5.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        label5.setText("Delete");
+        Panel_Inventory.add(label5, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 210, 130, 40));
+
+        jComboBox3.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        Panel_Inventory.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 120, 180, 30));
+
+        jPanel3.add(Panel_Inventory, "card3");
+
+        jPanel6.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel6.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel10.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("Supplier");
+        jPanel6.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 10, -1, -1));
+
+        jLabel17.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel17.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel17.setText("RFC");
+        jPanel6.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, -1, -1));
+
+        jLabel18.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel18.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel18.setText("Company");
+        jPanel6.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 60, -1, -1));
+
+        jTextField7.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jTextField7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField7ActionPerformed(evt);
+            }
+        });
+        jPanel6.add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 60, 190, 30));
+
+        jTextField8.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jPanel6.add(jTextField8, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 60, 140, 30));
+
+        jLabel19.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel19.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel19.setText("Address");
+        jPanel6.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 100, -1, -1));
+
+        jTextField9.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jPanel6.add(jTextField9, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 100, 190, 30));
+
+        jLabel20.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jLabel20.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel20.setText("Phone");
+        jPanel6.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
+
+        jTextField10.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        jPanel6.add(jTextField10, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 100, 140, 30));
+
+        label6.setAlignment(java.awt.Label.CENTER);
+        label6.setBackground(new java.awt.Color(0, 153, 51));
+        label6.setFont(new java.awt.Font("Cambria", 0, 24)); // NOI18N
+        label6.setText("Add");
+        jPanel6.add(label6, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 150, 130, 40));
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable3);
+
+        jPanel6.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 580, 260));
+
+        jPanel3.add(jPanel6, "card2");
+
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 0, 660, 490));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 860, 480));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField7ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -112,11 +387,55 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Panel_Inventory;
+    private javax.swing.JPanel Panel_shop;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTable jTable3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField10;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
+    private javax.swing.JTextField jTextField8;
+    private javax.swing.JTextField jTextField9;
+    private java.awt.Label label1;
+    private java.awt.Label label2;
+    private java.awt.Label label3;
+    private java.awt.Label label4;
+    private java.awt.Label label5;
+    private java.awt.Label label6;
     // End of variables declaration//GEN-END:variables
 }
